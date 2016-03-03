916a20b6642606b5d85104b4d69ecc9c92460f84
cretus <hessoucretus@gmail.com>
2016-03-03 16:53:43 +0100

    new commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('PageAccueil', { title: 'Bienvenue sur notre site' });
 });
 router.get('/test',function(req,res,next){
   res.render('PageAccueil', { title: 'Bienvenue sur Notre site' });
